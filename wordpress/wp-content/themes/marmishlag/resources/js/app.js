921ca89f0bb877bf4243a9cb59fcdb08308d140e
dorian-vidal <dorian.vidal@hetic.net>
2022-03-23 16:59:26 +0100

    feat(Login):add login/register page

--- wordpress/wp-content/themes/marmishlag/resources/js/app.js
+++ wordpress/wp-content/themes/marmishlag/resources/js/app.js
@@ -1,11 +1,21 @@
 // Navigation toggle
-jQuery(document).ready(function () {
+jQuery(document).ready(function ($) {
+  const main_navigation = jQuery("#primary-menu");
 
-      const main_navigation = jQuery('#primary-menu');
+  jQuery("#primary-menu-toggle").on("click", function (e) {
+    e.preventDefault();
 
-      jQuery('#primary-menu-toggle').on('click', function (e) {
-            e.preventDefault();
-
-            main_navigation.toggleClass('hidden');
-      });
+    main_navigation.toggleClass("hidden");
+  });
+  $(function () {
+    $(".btn").click(function () {
+      $(".form-signin").toggleClass("form-signin-left");
+      $(".form-signup").toggleClass("form-signup-left");
+      $(".frame").toggleClass("frame-long");
+      $(".signup-inactive").toggleClass("signup-active");
+      $(".signin-active").toggleClass("signin-inactive");
+      $(".forgot").toggleClass("forgot-left");
+      $(this).removeClass("idle").addClass("active");
+    });
+  });
 });
