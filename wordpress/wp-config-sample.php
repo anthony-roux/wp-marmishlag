7a6f5573decd1e55b7d19b8dd53848f1a5b25129
InspecterCodeRicky <ricky100198@gmail.com>
2022-04-06 12:17:21 +0200

    add plugin for wishlist

--- wordpress/wp-config-sample.php
+++ wordpress/wp-config-sample.php
@@ -86,7 +86,11 @@ $table_prefix = 'wp_';
  *
  * @link https://fr.wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+// define( 'WP_DEBUG', false );
+ini_set('display_errors','Off');
+ini_set('error_reporting', E_ALL );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
 
 /* C’est tout, ne touchez pas à ce qui suit ! Bonne publication. */
 
